f2d1a6b3bdc2c676c1204aeaa771cf5dd810e222
Abdallah Raheem <159222387+a8da11ah@users.noreply.github.com>
2025-06-08 22:52:19 +0300

    fix mongoDB connection and adjust it

--- src/config/mongoDBconnection.js
+++ src/config/mongoDBconnection.js
@@ -5,13 +5,21 @@ const connectDb = async () => {
 
     try {
         const conn =await mongoose.connect(process.env.MONGO_URI)
-        console.log("Database connected")
+        if(process.env.NODE_ENV === 'development') {
+         console.log("Database connected")
         console.log(
             `MongoDB Connected to 
             host: ${conn.connection.host}, 
             port: ${conn.connection.port},
             name: ${conn.connection.name}`);
-        
+        }else{
+        mongoose.set('debug', false) // disable debug mode in production
+        }
+
+
+
+
+
     } catch (error) {
         console.log(`error: ${error.message}`)
         process.exit(1) //exit with failure code 1 , 0 is success
