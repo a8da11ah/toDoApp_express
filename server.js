5232ec3c87d2c7bf616bdc344a7054b2cf6baf62
Abdallah Raheem <159222387+a8da11ah@users.noreply.github.com>
2025-06-08 22:23:02 +0300

    prepare the project to be hosted on netlify

--- server.js
+++ server.js
@@ -1,50 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
-import connectDb from './config/mongoDBconnection.js';
-import cookieParser from 'cookie-parser';
-import errorHandler from './middlewares/errorHandler.js';
-
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger.js';
-
-
-
-const app = express();
-
-
-// Middleware
-
-// allow us to access req.body in json
-app.use(express.json()) 
-
-// allow us to access req.body in form data
-app.use(express.urlencoded({extended:false}))
-
-
-// allow us to access cookies
-app.use(cookieParser())
-
-// Routes
-import apiRouter from './routes/apiRoute.js';
-
-// app.use('/api', (await import('./routes/apiRoute.js')).default);
-
-app.use('/api', apiRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
-
-
-// error handling middleware
-app.use(errorHandler)
-
-
-
+import app from './src/app.js';
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    connectDb()
+    
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ src/app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDb from './config/mongoDBconnection.js';
+import cookieParser from 'cookie-parser';
+import errorHandler from './middlewares/errorHandler.js';
+
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger.js';
+
+
+
+const app = express();
+connectDb()
+
+
+// Middleware
+
+// allow us to access req.body in json
+app.use(express.json()) 
+
+// allow us to access req.body in form data
+app.use(express.urlencoded({extended:false}))
+
+
+// allow us to access cookies
+app.use(cookieParser())
+
+// Routes
+import apiRouter from './routes/apiRoute.js';
+
+// app.use('/api', (await import('./routes/apiRoute.js')).default);
+
+app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
+
+// error handling middleware
+app.use(errorHandler)
+
+
+
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+    
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app;
